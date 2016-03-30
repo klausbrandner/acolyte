799cbdd49aa1df2b9afa0d963c841ef8275b2913
5-designs <k.brandner@5-designs.com>
2016-03-30 10:55:14 +0200

    UI changes

--- assets/js/messagebox/directives.js
+++ assets/js/messagebox/directives.js
@@ -1,15 +1,15 @@
 (function(){
-    
+
     angular.module('Acolyte')
-    
+
     .directive('acoMessageBox',function(){
         return{
             restrict: 'E',
             controller: 'AcoMessageBoxController',
             controllerAs: 'acoMBCtrl',
             scope: {},
-            templateUrl: acolyte.pathToAcolyte + 'templates/aco-message-box.html'
+            templateUrl: acolyte.pathToAcolyte + 'templates/message-box.html'
         };
     })
-    
+
 })();
