b5a71bb9a2c290fa45d5c84897b061e6a6b636d9
Phan <stadlerphilipp.phandawg@gmail.com>
2016-04-13 19:26:08 +0200

    security token

--- acolyte/server/func/security_csrf.php
+++ acolyte/server/func/security_csrf.php
@@ -2,10 +2,12 @@
 require_once("settings.php"); 
 
 function security_token($token){
-    if(CSRF === true){    
-        if($token === $_COOKIE['aco-token']) return true;
+    if(CSRF === true){
+        if(isset($_COOKIE['aco-token'])){
+            if($token === $_COOKIE['aco-token']) return true;
+        }else return false;
     }
-    return false;
+    return true;
 }
 
 function security_login($user, $password){
